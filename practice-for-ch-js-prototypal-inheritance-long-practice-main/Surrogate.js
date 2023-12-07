65bd4dc2b91b9e5d13fbade9cf671ddd09b0bc28
Jacob Tabor <jtabor214@gmail.com>
2023-12-07 17:06:59 -0500

    Complete Surrogate.js

---
+++ practice-for-ch-js-prototypal-inheritance-long-practice-main/Surrogate.js
@@ -0,0 +1,7 @@
+Function.prototype.inherits = function(Parent){
+    function Surrogate () {}
+    Surrogate.prototype = Parent.prototype;
+    this.prototype = new Surrogate();
+    this.prototype.constructor = this;
+};
+
